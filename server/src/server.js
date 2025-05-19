7a13c07ef757c296ef585ac9a7f6fbd1b151ad89
rasel <you@example.com>
2025-05-19 17:50:49 +0600

    'backend'

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,12 @@ app.use(cookieParser());
 const isLoggedIn = (req, res, next) => {
     const login = true; 
     if (login) {
-        req.body = { id: 101 };  
+        req.body = {
+            id: 101,
+            username: 'user',
+            email: 'user@gmail.com',
+            role: 'user'
+        };  
         next();
     } else {
         return res.status(401).json({
@@ -31,10 +36,16 @@ app.get('/', (req, res) => {
 // Protected route for user profile
 app.get('/api/user', isLoggedIn, (req, res) => {
     const userId = req.body.id;
+    const username = req.body.username;
+    const email = req.body.email;
+    const role = req.body.role;
     console.log('userId :',userId);
     res.status(200).json({  // json and send working same for data send to api
         message: 'This is the user profile',
         userId: userId,
+        userName: username,
+        email : email,
+        role: role
     });
 });
 // 404 rout if find
